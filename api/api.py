77e13597182f1765e7e104bc399b98caa13c16a7
omrfrq <omr.frq1999@gmail.com>
2022-07-31 21:44:30 +0500

    "multiple changes"

--- api/api.py
+++ api/api.py
@@ -1,7 +1,11 @@
 from flask import Flask
 import time
-app=Flask(__name__)
+app=Flask(__name__, static_folder="../build",static_url_path='/')
 
-@app.route('/time')
+@app.route('/api/time')
+
+def index():
+    return app.send_static_file('index.html')
+    
 def get_current_time():
  return {'time':time.time()}
